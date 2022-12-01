ce7c51cec1bcbdd7c40db7bd29d7fb9b1689803d
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-01 09:09:53 +0100

    feat: solve day 1 of the advent of code

---
+++ d01-calorie-counting/src/main.rs
@@ -0,0 +1,40 @@
+use itertools::Itertools;
+
+/// Convert the input string to a vector.
+///
+/// This function converts the input file to a vector of integers storing the sum of calories each
+/// elf is carrying.
+fn get_calories_per_elf(input: &str) -> Vec<u64> {
+    input
+        .split_terminator('\n')
+        .fold(Vec::new(), |mut acc, x| {
+            if x.is_empty() || acc.is_empty() {
+                acc.push(Vec::new());
+            }
+            acc.last_mut().unwrap().push(x);
+            acc
+        })
+        .iter()
+        .map(|v| {
+            v.iter()
+                .fold(0, |acc, x| acc + x.parse::<u64>().unwrap_or(0))
+        })
+        .collect()
+}
+
+fn main() {
+    let input = get_calories_per_elf(include_str!("../input.txt"));
+
+    let max = input.iter().max().unwrap_or(&0);
+    let sum = input
+        .iter()
+        .sorted_by(|a, b| b.cmp(a))
+        .take(3)
+        .fold(0, |acc, &x| acc + x);
+
+    println!("The elf carrying the most calories has {} calories.", max);
+    println!(
+        "The sum of the calories carried by the top 3 elves is {}",
+        sum
+    );
+}
