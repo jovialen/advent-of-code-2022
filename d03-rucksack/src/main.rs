daf950472b48920d889f70590fc047a7a213d2b1
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-03 13:37:16 +0100

    refactor: clean up day 3 solutions

--- d03-rucksack/src/main.rs
+++ d03-rucksack/src/main.rs
@@ -7,27 +7,22 @@ fn priority(c: char) -> u64 {
 }
 
 fn main() {
-    let input = include_str!("../input.txt");
+    let rucksacks: Vec<_> = include_str!("../input.txt").lines().collect();
 
-    let rucksack_priority_sum: u64 = input
-        .lines()
+    let rucksack_priority_sum = rucksacks
+        .iter()
         .map(|line| line.split_at(line.len() / 2))
-        .map(|(left, right)| left.chars().find(|&c| right.contains(c)))
-        .filter_map(|v| v)
-        .map(|c| priority(c))
-        .sum();
+        .filter_map(|(left, right)| left.chars().find(|&c| right.contains(c)))
+        .fold(0, |acc, c| acc + priority(c));
 
-    let groups_priority_sum: u64 = input
-        .lines()
-        .collect::<Vec<_>>()
+    let groups_priority_sum = rucksacks
         .chunks_exact(3)
         .filter_map(|chunk| {
             chunk[0]
                 .chars()
                 .find(|&c| chunk[1].contains(c) && chunk[2].contains(c))
         })
-        .map(|c| priority(c))
-        .sum();
+        .fold(0, |acc, c| acc + priority(c));
 
     println!("Sum of Rucksack priorities: {}", rucksack_priority_sum);
     println!("Sum of group priorities: {}", groups_priority_sum);
