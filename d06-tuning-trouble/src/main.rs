4f560e231001389c2eb3ad804126d646580476ed
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-06 09:41:20 +0100

    refactor: optimize unique sequence check

--- d06-tuning-trouble/src/main.rs
+++ d06-tuning-trouble/src/main.rs
@@ -1,12 +1,14 @@
-use std::collections::HashSet;
-
 const PACKET_HEADER_START_LEN: usize = 4;
 const PACKET_MESSAGE_START_LEN: usize = 14;
 
 fn find_unique_sequence(bytes: &[u8], unique_count: usize) -> Option<(usize, String)> {
     let offset = bytes.windows(unique_count).position(|window| {
-        let mut unique = HashSet::new();
-        window.iter().all(move |v| unique.insert(v))
+        for (i, x) in window.iter().enumerate() {
+            if window[i + 1..].iter().any(|y| x == y) {
+                return false;
+            }
+        }
+        true
     })?;
 
     let sequence = bytes[offset..offset + unique_count]
