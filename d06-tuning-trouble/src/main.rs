31912dd8ce877202441d3f004eb431db88adf56e
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-06 09:14:58 +0100

    refactor: clean up day 6 solutions

--- d06-tuning-trouble/src/main.rs
+++ d06-tuning-trouble/src/main.rs
@@ -1,36 +1,42 @@
 use std::collections::HashSet;
 
-fn find_unique_sequence(bytes: &[u8], unique_count: usize) -> usize {
-    bytes
-        .windows(unique_count)
-        .position(|window| {
-            let mut unique = HashSet::new();
-            window.iter().all(move |v| unique.insert(v))
-        })
-        .unwrap()
+const PACKET_HEADER_START_LEN: usize = 4;
+const PACKET_MESSAGE_START_LEN: usize = 14;
+
+fn find_unique_sequence(bytes: &[u8], unique_count: usize) -> Option<(usize, String)> {
+    let offset = bytes.windows(unique_count).position(|window| {
+        let mut unique = HashSet::new();
+        window.iter().all(move |v| unique.insert(v))
+    })?;
+
+    let sequence = bytes[offset..offset + unique_count]
+        .iter()
+        .map(|&v| v as char)
+        .collect::<String>();
+
+    Some((offset, sequence))
 }
 
 fn main() {
     let input = include_bytes!("../input.txt");
 
-    let header_offset = find_unique_sequence(input, 4);
-    let message_offset = find_unique_sequence(input, 14);
+    if let Some((offset, seq)) = find_unique_sequence(input, PACKET_HEADER_START_LEN) {
+        println!(
+            "Start of packet header: {}",
+            offset + PACKET_HEADER_START_LEN
+        );
+        println!("Header start: {}", seq);
+    } else {
+        println!("No packet header.");
+    }
 
-    println!("Start of packet header: {}", header_offset + 4);
-    println!(
-        "Header start: {:?}",
-        input[header_offset..header_offset + 4]
-            .iter()
-            .map(|v| *v as char)
-            .collect::<String>()
-    );
-
-    println!("Start of packet message: {}", message_offset + 14);
-    println!(
-        "Message start: {:?}",
-        input[message_offset..message_offset + 14]
-            .iter()
-            .map(|v| *v as char)
-            .collect::<String>()
-    );
+    if let Some((offset, seq)) = find_unique_sequence(input, PACKET_MESSAGE_START_LEN) {
+        println!(
+            "Start of packet message: {}",
+            offset + PACKET_MESSAGE_START_LEN
+        );
+        println!("Message start: {}", seq);
+    } else {
+        println!("No packet message.");
+    }
 }
