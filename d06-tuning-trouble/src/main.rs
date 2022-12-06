f3ec2a2d00da1648f0dbee625ada96b27689e71f
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-06 09:08:23 +0100

    feat: solve task 2 of day 6

--- d06-tuning-trouble/src/main.rs
+++ d06-tuning-trouble/src/main.rs
@@ -1,19 +1,34 @@
 use std::collections::HashSet;
 
-fn main() {
-    let input = include_bytes!("../input.txt");
-    let offset = input
-        .windows(4)
+fn find_unique_sequence(bytes: &[u8], unique_count: usize) -> usize {
+    bytes
+        .windows(unique_count)
         .position(|window| {
             let mut unique = HashSet::new();
             window.iter().all(move |v| unique.insert(v))
         })
-        .unwrap();
+        .unwrap()
+}
 
-    println!("Start of packet header: {}", offset);
+fn main() {
+    let input = include_bytes!("../input.txt");
+
+    let header_offset = find_unique_sequence(input, 4);
+    let message_offset = find_unique_sequence(input, 14);
+
+    println!("Start of packet header: {}", header_offset + 4);
     println!(
-        "Header: {:?}",
-        input[offset..offset + 4]
+        "Header start: {:?}",
+        input[header_offset..header_offset + 4]
+            .iter()
+            .map(|v| *v as char)
+            .collect::<String>()
+    );
+
+    println!("Start of packet message: {}", message_offset + 14);
+    println!(
+        "Message start: {:?}",
+        input[message_offset..message_offset + 14]
             .iter()
             .map(|v| *v as char)
             .collect::<String>()
