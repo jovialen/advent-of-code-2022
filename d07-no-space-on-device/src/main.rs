8a132607030213617951b63ced54011864044e62
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-07 13:32:24 +0100

    refactor: rewrite parse func for readability

--- d07-no-space-on-device/src/main.rs
+++ d07-no-space-on-device/src/main.rs
@@ -7,35 +7,36 @@ enum Entry {
 }
 
 fn parse_input(input: &str) -> HashMap<PathBuf, Vec<Entry>> {
-    let mut path = PathBuf::from("/");
+    let mut path = PathBuf::new();
     let mut filesystem = HashMap::new();
 
-    for mut line in input.lines().map(|line| line.split_ascii_whitespace()) {
-        match line.next() {
-            Some("$") => match line.next() {
-                Some("cd") => match line.next() {
-                    Some("..") => {
-                        path.pop();
+    for line in input.lines().filter(|line| !line.is_empty()) {
+        if line.starts_with("$") {
+            match line[2..].split_at(2) {
+                ("cd", " ..") => {
+                    path.pop();
+                }
+                ("cd", dirname) => {
+                    path.push(dirname.trim());
+                    if !filesystem.contains_key(&path) {
+                        filesystem.insert(path.clone(), Vec::new());
                     }
-                    Some(dir) => {
-                        path.push(dir);
-                        if !filesystem.contains_key(&path) {
-                            filesystem.insert(path.clone(), Vec::new());
-                        }
-                    }
-                    None => unreachable!(),
-                },
-                _ => (),
-            },
-            Some("dir") => filesystem
-                .get_mut(&path)
-                .unwrap()
-                .push(Entry::Directory(line.next().unwrap().to_string())),
-            Some(file_size) => filesystem
-                .get_mut(&path)
-                .unwrap()
-                .push(Entry::File(file_size.parse().unwrap())),
-            None => (),
+                }
+                ("ls", _) => (),
+                (command, _) => unreachable!("Unknown command \"{}\"", command),
+            }
+        } else {
+            match line.split_once(char::is_whitespace) {
+                Some(("dir", dirname)) => filesystem
+                    .get_mut(&path)
+                    .unwrap()
+                    .push(Entry::Directory(dirname.to_string())),
+                Some((size, _filename)) => filesystem
+                    .get_mut(&path)
+                    .unwrap()
+                    .push(Entry::File(size.parse().unwrap_or(0))),
+                None => unreachable!(),
+            }
         }
     }
 
@@ -60,6 +61,7 @@ fn size_of_dir(filesystem: &HashMap<PathBuf, Vec<Entry>>, dir: &PathBuf) -> usiz
 
 fn main() {
     let input = parse_input(include_str!("../input.txt"));
+
     let entry_sizes = input
         .keys()
         .map(|key| size_of_dir(&input, &key))
