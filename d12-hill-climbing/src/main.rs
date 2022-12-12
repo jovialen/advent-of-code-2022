13c72d19126b8fca4a45233ffb4e24e30f0cefdc
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-12 20:11:48 +0100

    feat: solve task 2 of day 12

--- d12-hill-climbing/src/main.rs
+++ d12-hill-climbing/src/main.rs
@@ -25,7 +25,7 @@ impl Position {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Map {
     data: Vec<u8>,
     width: usize,
@@ -151,6 +151,28 @@ fn main() {
     let map = Map::from(include_str!("../input.txt"));
 
     let len_of_path = a_star(&map).expect("Failed to find a valid path").len() - 1;
+    let len_of_hike = map
+        .data
+        .iter()
+        .enumerate()
+        .filter(|(_, &v)| v == 0)
+        .map(|(i, _)| {
+            let mut tmp_map = map.clone();
+            tmp_map.start = Position::from_index(i, tmp_map.width);
 
-    println!("Length of shortest path to destination: {}", len_of_path);
+            a_star(&tmp_map)
+                .and_then(|v| Some(v.len() - 1))
+                .unwrap_or(usize::MAX)
+        })
+        .min()
+        .expect("Failed to find any hike from lowest elevation to destination");
+
+    println!(
+        "Length of path to destination (no climbing): {}",
+        len_of_path
+    );
+    println!(
+        "Length of path to destination (with climbing): {}",
+        len_of_hike
+    );
 }
