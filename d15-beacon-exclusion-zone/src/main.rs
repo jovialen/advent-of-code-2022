a86724c45750a79ec5ae1f91224a55c20c58c63d
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-15 22:28:47 +0100

    feat: solve task 2 of day 15

--- d15-beacon-exclusion-zone/src/main.rs
+++ d15-beacon-exclusion-zone/src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use std::collections::HashSet;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Position(i64, i64);
 
 impl Position {
@@ -48,6 +50,7 @@ fn parse_input(input: &str) -> Vec<Sensor> {
 fn main() {
     let sensors = parse_input(include_str!("../input.txt"));
     let y = 2_000_000;
+    let size = 4_000_000;
 
     let min_x = sensors.iter().map(|sensor| sensor.beacon.0).min().unwrap();
     let max_x = sensors.iter().map(|sensor| sensor.beacon.0).max().unwrap();
@@ -65,4 +68,37 @@ fn main() {
         .count();
 
     println!("Count of blocked tiles: {}", count);
+
+    let points_of_interest = sensors
+        .iter()
+        .flat_map(|sensor| {
+            let min_x = sensor.position.0 - sensor.distance;
+            let max_x = sensor.position.0 + sensor.distance - 1;
+            let min_y = sensor.position.1 - sensor.distance;
+            let max_y = sensor.position.1 + sensor.distance - 1;
+
+            (min_x..=max_x)
+                .chain((min_x..max_x).rev())
+                .zip(
+                    (min_y..=max_y)
+                        .cycle()
+                        .skip_while(|&i| i != sensor.position.1 - 1),
+                )
+                .filter(|&(x, y)| x <= size && y <= size && x >= 0 && y >= 0)
+                .map(|(x, y)| Position(x, y))
+        })
+        .collect::<HashSet<_>>();
+
+    let distress_beacon_pos = points_of_interest
+        .iter()
+        .find(|point| {
+            !sensors
+                .iter()
+                .any(|sensor| sensor.position.distance(**point) <= sensor.distance)
+        })
+        .unwrap();
+
+    let tuning_freq = distress_beacon_pos.0 * 4000000 + distress_beacon_pos.1;
+
+    println!("Tuning frequency of distress beacon: {}", tuning_freq);
 }
