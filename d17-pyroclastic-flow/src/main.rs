5b7fadf61e4b8d953a4a7c6d2f8bbe5f68efead1
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-17 15:37:18 +0100

    refactor: dont push empty lines to tower

--- d17-pyroclastic-flow/src/main.rs
+++ d17-pyroclastic-flow/src/main.rs
@@ -56,16 +56,13 @@ fn shift_tetrino(tetrino: u32, dir: &Direction) -> u32 {
     }
 }
 
-fn main() {
-    let jetstreams = parse_input(include_str!("../input.txt"));
-    // let jetstreams = parse_input(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>");
-
+fn simulate(rock_count: usize, directions: &Vec<Direction>) -> usize {
     let mut tower: Vec<u8> = vec![0xFF];
-    let mut next_stream = jetstreams.iter().cycle();
     let mut next_tetrino = TETRINOS.iter().cycle();
-    for _ in 0..2022 {
+    let mut next_stream = directions.iter().cycle();
+    for _ in 0..rock_count {
         let mut tetrino = *next_tetrino.next().unwrap();
-        for y in (1..tower.iter().filter(|&&line| line != 0).count() + 4).rev() {
+        for y in (1..tower.len() + 4).rev() {
             let map_line_3 = *tower.get(y + 3).unwrap_or(&0);
             let map_line_2 = *tower.get(y + 2).unwrap_or(&0);
             let map_line_1 = *tower.get(y + 1).unwrap_or(&0);
@@ -81,28 +78,33 @@ fn main() {
             };
 
             if next_id & tetrino != 0 {
-                while tower.len() <= y + 3 {
-                    tower.push(0);
+                for (i, &res_line) in (map_id | tetrino).to_le_bytes().iter().enumerate() {
+                    if res_line == 0 {
+                        break;
+                    }
+
+                    if y + i >= tower.len() {
+                        tower.push(res_line);
+                    } else {
+                        tower[y + i] = res_line;
+                    }
                 }
-
-                let [res_line_3, res_line_2, res_line_1, res_line_0] =
-                    (map_id | tetrino).to_be_bytes();
-
-                *tower.get_mut(y + 3).unwrap() = res_line_3;
-                *tower.get_mut(y + 2).unwrap() = res_line_2;
-                *tower.get_mut(y + 1).unwrap() = res_line_1;
-                *tower.get_mut(y + 0).unwrap() = res_line_0;
 
                 break;
             }
         }
-
-        // println!();
-        // for line in tower.iter().rev() {
-        //     println!("{}", format!("{:#010b}", line).replace("0", " "));
-        // }
     }
 
-    let height = tower.iter().filter(|&&line| line != 0).count() - 1;
+    tower.iter().filter(|&&line| line != 0).count() - 1
+}
+
+fn main() {
+    // let jetstreams = parse_input(include_str!("../input.txt"));
+    let jetstreams = parse_input(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>");
+
+    let height = simulate(2022, &jetstreams);
+    println!("Height of tower after 2022 blocks: {}", height);
+
+    let height = simulate(1_000_000_000_000, &jetstreams);
     println!("Height of tower after 2022 blocks: {}", height);
 }
