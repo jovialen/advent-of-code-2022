470ecdb3238538573b832528ef6b845cb590b8c2
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2022-12-18 14:04:11 +0100

    refactor: add cache for evaluated points

--- d18-boiling-boulders/src/main.rs
+++ d18-boiling-boulders/src/main.rs
@@ -1,5 +1,8 @@
 use pathfinding::prelude::astar;
-use std::ops::{Add, Sub};
+use std::{
+    collections::HashMap,
+    ops::{Add, Sub},
+};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 struct Point3D(i32, i32, i32);
@@ -57,24 +60,32 @@ fn parse_input(input: &str) -> Vec<Point3D> {
 }
 
 fn surface_area(points: &Vec<Point3D>) -> usize {
+    let mut cache = HashMap::new();
+
     points.iter().fold(0, move |acc, point| {
         point.get_neighbours().iter().fold(acc, |acc, neighbour| {
             let is_air = !points.contains(&neighbour);
 
             let goal = Point3D(-1, -1, -1);
-            let is_free = astar(
-                neighbour,
-                |point| {
-                    point
-                        .get_neighbours()
-                        .into_iter()
-                        .filter(|point| !points.contains(point))
-                        .map(|point| (point, 1))
-                },
-                |point| point.distance(goal) as u32,
-                |&point| point == goal,
-            )
-            .is_some();
+            let is_free = if let Some(free) = cache.get(neighbour) {
+                *free
+            } else {
+                let free = astar(
+                    neighbour,
+                    |point| {
+                        point
+                            .get_neighbours()
+                            .into_iter()
+                            .filter(|point| !points.contains(point))
+                            .map(|point| (point, 1))
+                    },
+                    |point| point.distance(goal) as u32,
+                    |&point| point == goal,
+                )
+                .is_some();
+                cache.insert(*neighbour, free);
+                free
+            };
 
             if is_air && is_free {
                 acc + 1
